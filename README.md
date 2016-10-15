a505152013875e42a229266c36a47e9880c91440
jasonf7 <jason.z.fang@gmail.com>
2016-10-15 18:13:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Game Service #
+
+## Build & Run ##
+
+```sh
+$ cd Game_Service
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/com/shuttleql/services/game/GameServiceServlet.scala
@@ -0,0 +1,16 @@
+package com.shuttleql.services.game
+
+import org.scalatra._
+
+class GameServiceServlet extends GameServiceStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
