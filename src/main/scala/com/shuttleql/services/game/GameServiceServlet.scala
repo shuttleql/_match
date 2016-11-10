66b2c01bdd82815324205c435ecee2f9204115bd
Clement Hoang <clement.hoang24@gmail.com>
2016-11-09 23:04:07 -0500

    Add queue to root endpoint

--- src/main/scala/com/shuttleql/services/game/GameServiceServlet.scala
+++ src/main/scala/com/shuttleql/services/game/GameServiceServlet.scala
@@ -48,7 +48,10 @@ class GameServiceServlet extends GameServiceStack with JacksonJsonSupport {
   }
 
   get("/") {
-    Ok(MatchMaker.getMatches)
+    Ok(Map(
+      "matches" -> MatchMaker.getMatches,
+      "queue" -> MatchMaker.getQueue
+    ))
   }
 
   post("/checkedinplayers") {
